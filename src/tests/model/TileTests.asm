e2af55c38b53d23f5f56e87197f41778d3106110
Mark Bailey <markpdbailey@gmail.com>
2025-06-20 16:44:45 +0100

    Removed redundant code that has been replaced by the new tile functions

--- src/tests/model/TileTests.asm
+++ src/tests/model/TileTests.asm
@@ -68,21 +68,6 @@ UT_tilesToSprites1:
     db 4,"FISH",0,"AND",0,"CHIP",0,"FRIDAY",0
 
 
-
-UT_letterToSprite1:
-    ld a,10
-    ld (Tile.letterRow),a
-    ld a,15
-    ld (Tile.letterColumn),a
-    ld ix, spriteItemBuffer
-    ld a, 'S'
-    call Tile.letterToSprite
-    TEST_MEMORY_BYTE spriteItemBuffer+spriteItem.pattern,'S' - 57
-    TEST_MEMORY_BYTE spriteItemBuffer+spriteItem.x,16
-    TEST_MEMORY_BYTE spriteItemBuffer+spriteItem.x+1,1
-    TEST_MEMORY_BYTE spriteItemBuffer+spriteItem.y,192
-    TC_END
-
 UT_nextColumn1:
     ld a,10
     ld (Tile.letterRow),a
@@ -103,28 +88,6 @@ UT_nextColumn2:
     TEST_MEMORY_BYTE Tile.letterColumn,0
     TC_END
 
-UT_wordToSprites1:
-    call SpriteList.removeAll
-    ld a,5
-    ld (Tile.letterRow),a
-    ld a,7
-    ld (Tile.letterColumn),a
-    ld hl,wordAcorn
-    call Tile.wordToSprites
-    TEST_MEMORY_BYTE SpriteList.count,5
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem.pattern,'A' - 57
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem.id,0
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem.x,7*16+32
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem.x+1,0
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem.y,5*16+32
-
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem * 4 + spriteItem.pattern,'N' - 57
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem * 4 + spriteItem.id,4
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem * 4 + spriteItem.x,208
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem * 4 + spriteItem.x+1,0
-    TEST_MEMORY_BYTE SpriteList.list + spriteItem * 4 + spriteItem.y,112
-
-    TC_END
 
 ; In bounds
 UT_boundsCheck1:
