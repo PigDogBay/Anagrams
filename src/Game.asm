9aa89b5f721a01f94627dc8c910e1e2a8c82cde9
Mark Bailey <markpdbailey@gmail.com>
2025-06-26 16:50:23 +0100

    Tile now snaps to slot

--- src/Game.asm
+++ src/Game.asm
@@ -1,5 +1,7 @@
     module game
 
+TILE_SLOT_OVERLAP:              equ 12
+
 mouseStateJumpTable:
     dw stateMouseReady
     dw stateMouseHover
@@ -171,8 +173,51 @@ stateMouseDragEnd:
     ;Update mouse pointer pattern
     ld a,0
     ld (SpriteList.list + spriteItem.pattern),a
+    
+    call isTileOverSlot
+    or a
+    ret z
+    ;TODO if slot is occupied bounce tile downwards
+    ;
+    call Slot.snapTileToSlot
     ret
 
+;-----------------------------------------------------------------------------------
+;
+; Function: isTileOverSlot(uint16 ptrTile) -> uint16
+;
+; Searches through the sprites to find if the tile is over a slot sprite
+;
+;
+; Out: 
+;       A - 0 = not over slot, 1 = over slot 
+;       IY - matching slot sprite
+;
+; Dirty: A,B,DE,IX,IY
+;
+;-----------------------------------------------------------------------------------
+isTileOverSlot:
+    ld a, (SpriteList.count)
+    ;Discount mouse pointer and dragged tile sprites
+    dec a
+    dec a
+    ld b,a
+    ; Dragged tile has been brought to the front
+    ; so is at index 1
+    ld ix,SpriteList.list + spriteItem
+    ;Start searching from index 2
+    ld iy,SpriteList.list + spriteItem*2
+    ld de, spriteItem
+.next:
+    ld a, TILE_SLOT_OVERLAP
+    call SpriteList.collisionCheck
+    or a
+    jr nz, .collisionDetected
+    add iy,de
+    djnz .next
+
+.collisionDetected:
+    ret
 ;-----------------------------------------------------------------------------------
 ;
 ; addMouseSpritePointer
