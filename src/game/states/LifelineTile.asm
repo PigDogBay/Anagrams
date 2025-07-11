5eb6c81c70c682fdb97a580c7826fc1f1e77127e
Mark Bailey <markpdbailey@gmail.com>
2025-07-11 19:39:06 +0100

    Added mouse handler to LifelineTile

--- src/game/states/LifelineTile.asm
+++ src/game/states/LifelineTile.asm
@@ -23,6 +23,10 @@ enter:
     ld hl,instructionText2
     ld e, INSTRUCTION_ROW + 1
     call Game.printInstruction
+
+    //TODO convert tile sprites to clickable, not draggable
+    //convert slots to non-clickable
+
     ret
 
 
@@ -30,16 +34,67 @@ enter:
 
 update:
     ;wait for use to click mouse button
-    call Game.updateMouseNoSprite
-    cp MouseDriver.STATE_BACKGROUND_CLICKED
-    jr z, .mousePressed
+    call Game.updateMouse
+    call mouseStateHandler
     call Game.updateSprites
     ret
 
-.mousePressed:
+
+
+jumpTable:
+    dw stateMouseReady
+    dw stateMouseHover
+    dw stateMouseHoverEnd
+    dw stateMousePressed
+    dw stateMouseClicked
+    dw stateMouseDragStart
+    dw stateMouseDrag
+    dw stateMouseDragOutOfBounds
+    dw stateMouseDragEnd
+    dw stateMouseClickedOff
+    dw stateMouseBackgroundPressed
+    dw stateMouseBackgroundClicked
+
+;-----------------------------------------------------------------------------------
+;
+; Function: mouseStateHandler
+;
+; Updates the game based on the current mouse state 
+; In - A current mouse state
+;    - IX pointer to sprite that mouse is over
+;-----------------------------------------------------------------------------------
+mouseStateHandler:
+    ld hl, jumpTable
+    ; Add twice, as table is two bytes per entry
+    add hl,a
+    add hl,a
+    ; get jump entry
+    ld de,(hl)
+    ld hl,de
+    jp hl
+
+stateMouseReady:
+stateMouseHover:
+stateMouseHoverEnd:
+stateMousePressed:
+stateMouseDrag:
+stateMouseDragOutOfBounds:
+stateMouseDragEnd:
+stateMouseClickedOff:
+stateMouseBackgroundPressed:
+stateMouseDragStart:
+    ret
+
+stateMouseBackgroundClicked:
+    ; Cancel Lifeline
     ld hl, GS_PLAY
     call GameStateMachine.change
     ret
+
+stateMouseClicked:
+    nop
+    ret
+
 
 instructionText1:
     db "Select a Tile",0
