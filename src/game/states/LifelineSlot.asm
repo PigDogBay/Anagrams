b43425a3aa2030c4b40776c68d2951e83b5cf7f2
Mark Bailey <markpdbailey@gmail.com>
2025-07-15 13:13:06 +0100

    LifelineSlot done

--- src/game/states/LifelineSlot.asm
+++ src/game/states/LifelineSlot.asm
@@ -15,6 +15,7 @@ INSTRUCTION_ROW: equ 2
     stateStruct enter,update
 
 
+
 enter:
     ; Display instruction
     ld hl,instructionText1
@@ -23,20 +24,129 @@ enter:
     ld hl,instructionText2
     ld e, INSTRUCTION_ROW + 1
     call Game.printInstruction
+
+    ;Remove all interaction from sprites
+    call SpriteList.removeAllInteraction
+    ;Make the slot sprites clickable, to allow user to select one of them
+    call SpriteList.allSlotsClickable
+
+
     ret
+
+
+
 
 update:
     ;wait for use to click mouse button
-    call Game.updateMouseNoSprite
-    cp MouseDriver.STATE_BACKGROUND_CLICKED
-    jr z, .mousePressed
+    call Game.updateMouse
+    call mouseStateHandler
     call Game.updateSprites
     ret
 
-.mousePressed:
+
+
+jumpTable:
+    dw stateMouseReady
+    dw stateMouseHover
+    dw stateMouseHoverEnd
+    dw stateMousePressed
+    dw stateMouseClicked
+    dw stateMouseDragStart
+    dw stateMouseDrag
+    dw stateMouseDragOutOfBounds
+    dw stateMouseDragEnd
+    dw stateMouseClickedOff
+    dw stateMouseBackgroundPressed
+    dw stateMouseBackgroundClicked
+
+;-----------------------------------------------------------------------------------
+;
+; Function: mouseStateHandler
+;
+; Updates the game based on the current mouse state 
+; In - A current mouse state
+;    - IX pointer to sprite that mouse is over
+;-----------------------------------------------------------------------------------
+mouseStateHandler:
+    ld hl, jumpTable
+    ; Add twice, as table is two bytes per entry
+    add hl,a
+    add hl,a
+    ; get jump entry
+    ld de,(hl)
+    ld hl,de
+    jp hl
+
+stateMouseReady:
+stateMouseHover:
+stateMouseHoverEnd:
+stateMousePressed:
+stateMouseDrag:
+stateMouseDragOutOfBounds:
+stateMouseDragEnd:
+stateMouseClickedOff:
+stateMouseBackgroundPressed:
+stateMouseDragStart:
+    ret
+
+stateMouseBackgroundClicked:
+    ; Restore interaction flags
+    call SpriteList.restoreAllInteraction
+    ; Cancel Lifeline
     ld hl, GS_PLAY
     call GameStateMachine.change
     ret
+
+stateMouseClicked:
+    ;get slot's gameId
+    ld a,c
+    call Slot.find
+    ld a,h
+    or l
+    jr z, .notFound
+
+    ;save slotStruct ptr    
+    ld ix,hl
+
+    ;Find sprite and save it in HighlightSlot state
+    ld a,(ix+slotStruct.id)
+    call SpriteList.find
+    ld a,h
+    or l
+    jr z, .notFound
+    ;Found tile sprite
+    ld (GameState_HighlightSlot.slotSpritePtr),hl
+
+    ;IX points to slotStruct
+    ld a,(ix+slotStruct.letter)
+    call Tile.findByLetter
+    ld a,h
+    or l
+    jr z, .notFound
+
+    ;HL already points to gameId
+    ld a,(hl)
+    call SpriteList.find
+    ld a,h
+    or l
+    jr z, .notFound
+
+    ;Found matching slot
+    ld (GameState_HighlightSlot.tileSpritePtr),hl
+    ; Restore interaction flags
+    call SpriteList.restoreAllInteraction
+    ld hl, GS_HIGHLIGHT_SLOT
+    call GameStateMachine.change
+    ret
+
+.notFound:
+    ; Restore interaction flags
+    call SpriteList.restoreAllInteraction
+    ld hl, GS_PLAY
+    call GameStateMachine.change
+    ret
+
+
 
 instructionText1:
     db "Select a Slot",0
