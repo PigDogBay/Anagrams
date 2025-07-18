ab993320c4af4f292366e0158fa1f381a1f0a656
Mark Bailey <markpdbailey@gmail.com>
2025-07-18 11:12:39 +0100

    Manually add sprites method

--- src/game/states/Title.asm
+++ src/game/states/Title.asm
@@ -14,17 +14,12 @@
 
 enter:
     call NextSprite.removeAll
-    call SpriteList.removeAll
-    ld a,5
-    call Graphics.fillLayer2_320
-    ; First sprite always the mouse pointer so that it is on top
-    call Game.addMouseSpritePointer
+    call Graphics.titleScreen
 
-    call Tile.removeAll
-    ld hl,titleText
-    ld c, 16
-    call Tile.createTiles
-    call Tile.tilesToSprites
+    ld bc, spriteLen
+    ld de, SpriteList.count
+    ld hl, spriteData
+    ldir
 
     ret
 
@@ -42,6 +37,39 @@ update:
     ret
 
 titleText:
-    db "TITLE",0
+    db "THE\nSCHOLAR",0
+
+spriteData:
+    db 21
+    ; id, x, y, palette, pattern, gameId, flags
+    ; Mouse
+    spriteItem 0,160,128,0,0,0,0
+
+    ;Tile sprites
+    spriteItem 1,141,49,0,'T'-Tile.ASCII_PATTERN_OFFSET,1,0
+    spriteItem 2,161,49,0,'H'-Tile.ASCII_PATTERN_OFFSET,2,0
+    spriteItem 3,181,49,0,'E'-Tile.ASCII_PATTERN_OFFSET,3,0
+    spriteItem 4,101,73,0,'S'-Tile.ASCII_PATTERN_OFFSET,4,0
+    spriteItem 5,121,73,0,'C'-Tile.ASCII_PATTERN_OFFSET,5,0
+    spriteItem 6,141,73,0,'H'-Tile.ASCII_PATTERN_OFFSET,6,0
+    spriteItem 7,161,73,0,'O'-Tile.ASCII_PATTERN_OFFSET,7,0
+    spriteItem 8,181,73,0,'L'-Tile.ASCII_PATTERN_OFFSET,8,0
+    spriteItem 9,201,73,0,'A'-Tile.ASCII_PATTERN_OFFSET,9,0
+    spriteItem 10,221,73,0,'R'-Tile.ASCII_PATTERN_OFFSET,10,0
+
+    ;Slots
+    spriteItem 11,140,48,0,Slot.SLOT_SPRITE_PATTERN,11,0
+    spriteItem 12,160,48,0,Slot.SLOT_SPRITE_PATTERN,12,0
+    spriteItem 13,180,48,0,Slot.SLOT_SPRITE_PATTERN,13,0
+    spriteItem 14,100,72,0,Slot.SLOT_SPRITE_PATTERN,14,0
+    spriteItem 15,120,72,0,Slot.SLOT_SPRITE_PATTERN,15,0
+    spriteItem 16,140,72,0,Slot.SLOT_SPRITE_PATTERN,16,0
+    spriteItem 17,160,72,0,Slot.SLOT_SPRITE_PATTERN,17,0
+    spriteItem 18,180,72,0,Slot.SLOT_SPRITE_PATTERN,18,0
+    spriteItem 19,200,72,0,Slot.SLOT_SPRITE_PATTERN,19,0
+    spriteItem 20,220,72,0,Slot.SLOT_SPRITE_PATTERN,20,0
+
+
+spriteLen: equ $ - spriteData
 
     endmodule
