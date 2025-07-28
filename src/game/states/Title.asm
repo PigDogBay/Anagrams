f39fe0df5c1114d985d56c8cc8b5611844dc8c8e
Mark Bailey <markpdbailey@gmail.com>
2025-07-28 17:22:08 +0100

    Tweaked title animations

--- src/game/states/Title.asm
+++ src/game/states/Title.asm
@@ -45,16 +45,42 @@ initAppear:
     ld b, 18 : ld c, 40 : call Visibility.add
     ld b, 19 : ld c, 45 : call Visibility.add
     ld b, 20 : ld c, 50 : call Visibility.add
-    ld b, 1 : ld c, 55 : call Visibility.add
-    ld b, 2 : ld c, 60: call Visibility.add
-    ld b, 3 : ld c, 65 : call Visibility.add
-    ld b, 4 : ld c, 70 : call Visibility.add
-    ld b, 5 : ld c, 75 : call Visibility.add
-    ld b, 6 : ld c, 80 : call Visibility.add
-    ld b, 7 : ld c, 85 : call Visibility.add
-    ld b, 8 : ld c, 90 : call Visibility.add
-    ld b, 9 : ld c, 95 : call Visibility.add
-    ld b, 10 : ld c, 100 : call Visibility.add
+    ld b, 1 : ld c, 52 : call Visibility.add
+    ld b, 2 : ld c, 54: call Visibility.add
+    ld b, 3 : ld c, 56 : call Visibility.add
+    ld b, 4 : ld c, 58 : call Visibility.add
+    ld b, 5 : ld c, 60 : call Visibility.add
+    ld b, 6 : ld c, 62 : call Visibility.add
+    ld b, 7 : ld c, 64 : call Visibility.add
+    ld b, 8 : ld c, 66 : call Visibility.add
+    ld b, 9 : ld c, 68 : call Visibility.add
+    ld b, 10 : ld c, 70 : call Visibility.add
+    call Visibility.start
+    ret
+
+initDisappear:
+    call Visibility.removeAll
+    ;B - gameId, C - delay
+    ld b, 1 : ld c, 1 : call Visibility.add
+    ld b, 2 : ld c, 1 : call Visibility.add
+    ld b, 3 : ld c, 1 : call Visibility.add
+    ld b, 4 : ld c, 1 : call Visibility.add
+    ld b, 5 : ld c, 1 : call Visibility.add
+    ld b, 6 : ld c, 1 : call Visibility.add
+    ld b, 7 : ld c, 1 : call Visibility.add
+    ld b, 8 : ld c, 1 : call Visibility.add
+    ld b, 9 : ld c, 1 : call Visibility.add
+    ld b, 10 : ld c, 1 : call Visibility.add
+    ld b, 11 : ld c, 1 : call Visibility.add
+    ld b, 12 : ld c, 1: call Visibility.add
+    ld b, 13 : ld c, 1 : call Visibility.add
+    ld b, 14 : ld c, 1 : call Visibility.add
+    ld b, 15 : ld c, 1 : call Visibility.add
+    ld b, 16 : ld c, 1 : call Visibility.add
+    ld b, 17 : ld c, 1 : call Visibility.add
+    ld b, 18 : ld c, 1 : call Visibility.add
+    ld b, 19 : ld c, 1 : call Visibility.add
+    ld b, 20 : ld c, 1 : call Visibility.add
     call Visibility.start
     ret
 
@@ -70,6 +96,24 @@ initMoveTiles:
     pop bc
     djnz .loop
     call MoveSprites.start
+    ret
+
+initFlash:
+    xor a
+    ld (FlashSprites.index),a
+    ld hl, FlashSprites.idList
+    ld b,10
+    ld a,1
+.loop:
+    ld (hl),a
+    inc hl
+    inc a
+    djnz .loop
+    ld (hl),0
+
+    ld hl,150
+    call FlashSprites.start
+
     ret
 
 update:
@@ -126,14 +170,20 @@ titleMoveTiles:
     ld a, (Animator.finishedFlags)
     bit Animator.BIT_MOVE, a
     ret z
-    call initAppear
-    ld a,TITLE_STATE_FADE_OUT
+    call initFlash
+    ld a,TITLE_STATE_FLASH
     ld (titleState),a
     ret
 
 titleFlash:
+    ld a, (Animator.finishedFlags)
+    bit Animator.BIT_FLASH_SPRITES, a
+    ret z
+    call initDisappear
+    ld a,TITLE_STATE_FADE_OUT
+    ld (titleState),a
     ret
-    
+
 titleFadeOut:
     ;wait for fade in to finish
     ld a, (Animator.finishedFlags)
@@ -156,16 +206,16 @@ spriteData:
     spriteItem 0,160,128,0,0 | SPRITE_VISIBILITY_MASK,0,0
 
     ;Tile sprites
-    spriteItem 1,180,170,0,'T'-Tile.ASCII_PATTERN_OFFSET,1,0
-    spriteItem 2,120,195,0,'H'-Tile.ASCII_PATTERN_OFFSET,2,0
-    spriteItem 3,180,195,0,'E'-Tile.ASCII_PATTERN_OFFSET,3,0
-    spriteItem 4,201,168,0,'S'-Tile.ASCII_PATTERN_OFFSET,4,0
-    spriteItem 5,100,177,0,'C'-Tile.ASCII_PATTERN_OFFSET,5,0
+    spriteItem 1,181,171,0,'T'-Tile.ASCII_PATTERN_OFFSET,1,0
+    spriteItem 2,121,195,0,'H'-Tile.ASCII_PATTERN_OFFSET,2,0
+    spriteItem 3,181,195,0,'E'-Tile.ASCII_PATTERN_OFFSET,3,0
+    spriteItem 4,201,169,0,'S'-Tile.ASCII_PATTERN_OFFSET,4,0
+    spriteItem 5,101,177,0,'C'-Tile.ASCII_PATTERN_OFFSET,5,0
     spriteItem 6,219,183,0,'H'-Tile.ASCII_PATTERN_OFFSET,6,0
-    spriteItem 7,140,195,0,'O'-Tile.ASCII_PATTERN_OFFSET,7,0
-    spriteItem 8,160,195,0,'L'-Tile.ASCII_PATTERN_OFFSET,8,0
-    spriteItem 9,140,170,0,'A'-Tile.ASCII_PATTERN_OFFSET,9,0
-    spriteItem 10,160,170,0,'R'-Tile.ASCII_PATTERN_OFFSET,10,0
+    spriteItem 7,141,195,0,'O'-Tile.ASCII_PATTERN_OFFSET,7,0
+    spriteItem 8,161,195,0,'L'-Tile.ASCII_PATTERN_OFFSET,8,0
+    spriteItem 9,141,171,0,'A'-Tile.ASCII_PATTERN_OFFSET,9,0
+    spriteItem 10,161,171,0,'R'-Tile.ASCII_PATTERN_OFFSET,10,0
 
     ;Slots
     spriteItem 11,140,48,0,Slot.SLOT_SPRITE_PATTERN,11,0
@@ -183,15 +233,15 @@ spriteLen: equ $ - spriteData
 
 motionData:
     ; gameId, stepX, countX, stepY, countY, delay 
-    motionStruct 1, 1, 141, 1, 49, 90 
-    motionStruct 2, 1, 161, 1, 49, 60 
-    motionStruct 3, 1, 181, 1, 49, 70 
-    motionStruct 4, 1, 101, 1, 73, 80 
-    motionStruct 5, 1, 121, 1, 73, 10 
-    motionStruct 6, 1, 141, 1, 73, 100 
-    motionStruct 7, 1, 161, 1, 73, 40 
-    motionStruct 8, 1, 181, 1, 73, 20 
-    motionStruct 9, 1, 201, 1, 73, 50 
-    motionStruct 10, 1, 221, 1, 73, 30 
+    motionStruct 1, 2, 141, 2, 49, 90 
+    motionStruct 2, 2, 161, 2, 49, 60 
+    motionStruct 3, 2, 181, 2, 49, 70 
+    motionStruct 4, 2, 101, 2, 73, 80 
+    motionStruct 5, 2, 121, 2, 73, 10 
+    motionStruct 6, 2, 141, 2, 73, 100 
+    motionStruct 7, 2, 161, 2, 73, 40 
+    motionStruct 8, 2, 181, 2, 73, 20 
+    motionStruct 9, 2, 201, 2, 73, 50 
+    motionStruct 10, 2, 221, 2, 73, 30 
 
     endmodule
