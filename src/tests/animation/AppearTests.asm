45f5fe1da4ab1e868470715b5297f03e74ab645c
Mark Bailey <markpdbailey@gmail.com>
2025-07-28 15:55:30 +0100

    Tests for setVisibility

--- src/tests/animation/AppearTests.asm
+++ src/tests/animation/AppearTests.asm
@@ -19,6 +19,7 @@ UT_add1:
     TEST_MEMORY_BYTE Appear.list + 3,32
     TC_END
 
+;Make invisible
 UT_setVisibility1:
     ;set up sprites
     call SpriteList.removeAll
@@ -44,6 +45,31 @@ UT_setVisibility1:
 .sprite2:
     spriteItem 2, 200, 100, 0, 6 | SPRITE_VISIBILITY_MASK, 88, 0
 
+;Make visible
+UT_setVisibility2:
+    ;set up sprites
+    call SpriteList.removeAll
+    ld hl, .sprite1 : call SpriteList.addSprite
+    ld hl, .sprite2 : call SpriteList.addSprite
+
+    ;set up Appear data
+    call Appear.removeAll
+    ;B - gameId, C - delay
+    ld b, 42 : ld c, 55 : call Appear.add
+    ld b, 88 : ld c, 32 : call Appear.add
+
+    ld a,1
+    call Appear.setVisibility
+    ;Check visibility is clear
+    TEST_MEMORY_BYTE SpriteList.list + spriteItem.pattern, 5 | SPRITE_VISIBILITY_MASK
+    TEST_MEMORY_BYTE SpriteList.list + spriteItem + spriteItem.pattern, 6 | SPRITE_VISIBILITY_MASK
+    TC_END
+
+.sprite1:
+    ; id, x, y, palette, pattern, gameId, flags
+    spriteItem 1, 200, 100, 0, 5 , 42, 0
+.sprite2:
+    spriteItem 2, 200, 100, 0, 6 , 88, 0
 
 UT_update1:
     ;set up sprites
