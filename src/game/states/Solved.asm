0a9ba870930021291ddddeca7b7f58238aad0ef5
Mark Bailey <markpdbailey@gmail.com>
2025-09-03 11:56:06 +0100

    Solved now displays well done and flashes the tiles

--- src/game/states/Solved.asm
+++ src/game/states/Solved.asm
@@ -13,18 +13,36 @@
 
 
 enter:
-    call NextSprite.removeAll
-    call SpriteList.removeAll
-    ld a,6
-    call Graphics.fillLayer2_320
-    ; First sprite always the mouse pointer so that it is on top
-    call Game.addMouseSpritePointer
 
-    call Tile.removeAll
-    ld hl,titleText
-    ld c, 16
-    call Tile.createTiles
-    call Tile.tilesToSprites
+    ; Print "Well Done!"
+    ld d, 15
+    ld e, 16
+    ld hl,strWellDone
+    call Print.setCursorPosition
+    ld b,%00100000
+    call Print.printString
+
+    ;Print "You've Passed The Exam"
+    ld d, 9
+    ld e, 18
+    ld hl,strYouPassed
+    call Print.setCursorPosition
+    ld b,%00000000
+    call Print.printString
+
+    ;Print "Click To Continue"
+    ld d, 11
+    ld e, 29
+    ld hl,strClickToContinue
+    call Print.setCursorPosition
+    ;Green
+    ld b,%00010000
+    call Print.printString
+
+    call FlashSprites.copyAllTileIds
+    ld hl,300
+    call FlashSprites.start
+
 
     ret
 
@@ -50,12 +68,14 @@ update:
     call Puzzles.select
 
 .start:
-    ld hl, GS_START
+    ld hl, GS_ROUND
     call GameStateMachine.change
     ret
 
-titleText:
-    db "SOLVED",0
-
-
+strWellDone:
+    db "Well Done!",0
+strYouPassed:
+    db "You've Passed The Exam",0
+strClickToContinue:
+    db "Click to Continue",0
     endmodule
