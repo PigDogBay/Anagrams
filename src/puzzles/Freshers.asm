ff5db09b9d45d8751e03a991e45ef120a9f34be0
Mark Bailey <markpdbailey@gmail.com>
2025-09-12 17:57:43 +0100

    Freshers anagrams

---
+++ src/puzzles/Freshers.asm
@@ -0,0 +1,268 @@
+;-----------------------------------------------------------------------------------
+; 
+; Data: Freshers 
+; 
+; Simple anagrams for the first year
+; 
+;-----------------------------------------------------------------------------------
+
+    module CatFreshers
+
+table:
+    dw p1,p2,p3,p4,p5,p6,p7,p8,p9,p10
+    dw p11,p12,p13,p14,p15,p16,p17,p18,p19,p20
+    dw p21,p22,p23,p24,p25,p26,p27,p28,p29,p30
+    dw p31,p32,p33,p34,p35,p36,p37,p38,p39,p40
+    dw p41,p42,p43,p44,p45,p46,p47,p48,p49,p50
+
+p1: db "OCCUPATIONS",0
+    db "NURSE",0
+    db "DOCTOR",0
+    db "DENTIST",0
+
+p2: db "OCCUPATIONS",0
+    db "BIN MAN",0
+    db "POSTMAN",0
+    db "SHOP\nKEEPER",0
+
+p3: db "PRECIOUS METALS",0
+    db "SILVER",0
+    db "ROSE GOLD",0
+    db "PLATINUM",0
+
+p4: db "GEM STONES",0
+    db "DIAMOND",0
+    db "EMERALD",0
+    db "SAPPHIRE",0
+
+p5: db "CALENDAR MONTHS",0
+    db "JANUARY\n",0
+    db "FEBRUARY\n",0
+    db "DECEMBER\n",0
+
+p6: db "PALINDROMES",0
+    db "KAYAK",0
+    db "RACECAR",0
+    db "MADAM\nIM ADAM",0
+
+p7: db "COLOURS",0
+    db "INDIGO",0
+    db "VIOLET",0
+    db "MAGENTA",0
+
+p8: db "FAST FOOD",0
+    db "BURGER",0
+    db "DONER KEBAB",0
+    db "SAUSAGE ROLL",0
+
+p9: db "FRUIT",0
+    db "ORANGE",0
+    db "APRICOT",0
+    db "PINEAPPLE",0
+
+p10: db "VEGETABLES C...",0
+    db "CARROT",0
+    db "CABBAGE",0
+    db "CAULIFLOWER",0
+
+p11: db "VEGETABLES B...",0
+    db "BEETROOT",0
+    db "BROCCOLI",0
+    db "BROAD BEANS",0
+
+p12: db "CHILI PEPPERS",0
+    db "JALEPENO",0
+    db "BIRDS EYE",0
+    db "HABANERO",0
+
+p13: db "UK CITIES B...",0
+    db "BRISTOL",0
+    db "BELFAST",0
+    db "BIRMINGHAM",0
+
+p14: db "UK CITIES S...",0
+    db "SWANSEA",0
+    db "SHEFFIELD",0
+    db "STOKE ON\nTRENT",0
+
+p15: db "ON THE FARM",0
+    db "TRACTOR",0
+    db "CHICKEN",0
+    db "CLARKSON",0
+
+p16: db "MONSTERS",0
+    db "ZOMBIE",0
+    db "BANSHEE",0
+    db "BOGEYMAN",0
+
+p17: db "MONSTERS",0
+    db "HYDRA",0
+    db "CHIMERA",0
+    db "LOCH NESS",0
+
+p18: db "MONSTERS",0
+    db "YETI",0
+    db "BIGFOOT",0
+    db "SASQUATCH",0
+
+p19: db "MYTHICAL CREATURES",0
+    db "PHOENIX",0
+    db "GRIFFIN",0
+    db "MINOTAUR",0
+
+p20: db "MYTHICAL CREATURES",0
+    db "NYMPH",0
+    db "SPRITE",0
+    db "FAERIE",0
+
+p21: db "TRANSPORT",0
+    db "MOPED",0
+    db "BICYCLE",0
+    db "MOTOR BIKE",0
+
+p22: db "PATRON SAINTS",0
+    db "ST DAVID",0
+    db "ST ANDREW",0
+    db "ST GEORGE",0
+
+p23: db "US STATES",0
+    db "NEW YORK",0
+    db "NEW MEXICO",0
+    db "NEW JERSEY",0
+
+p24: db "US STATES",0
+    db "ALASKA",0
+    db "ALABAMA",0
+    db "ARKANSAS",0
+
+p25: db "US STATES",0
+    db "IDAHO",0
+    db "INDIANA",0
+    db "ILLINOIS",0
+
+p26: db "US STATES",0
+    db "MAINE",0
+    db "MONTANA",0
+    db "MISSISSIPPI",0
+
+p27: db "US STATES",0
+    db "VERMOUNT",0
+    db "VIRGINIA",0
+    db "WEST\nVIRGINIA",0
+
+p28: db "COUNTY TOWNS",0
+    db "DURHAM",0
+    db "WARWICK",0
+    db "STAFFORD",0
+
+p29: db "COUNTY TOWNS",0
+    db "OXFORD",0
+    db "NOTTINGHAM",0
+    db "WORCESTER",0
+
+p30: db "SCI-FI ACTION FILMS",0
+    db "ALIENS",0
+    db "ROBOCOP",0
+    db "THE MATRIX",0
+
+p31: db "US SITCOMS",0
+    db "CHEERS",0
+    db "FRIENDS",0
+    db "SEINFELD",0
+
+p32: db "UK SITCOMS",0
+    db "HI DE HI",0
+    db "DADS ARMY",0
+    db "YOU RANG\nME LORD",0
+
+p33: db "US COP SHOWS",0
+    db "COLUMBO",0
+    db "THE WIRE",0
+    db "MIAMI VICE",0
+
+p34: db "UK COP SHOWS",0
+    db "Z CARS",0
+    db "THE BILL",0
+    db "TAGGART",0
+
+p35: db "TREES",0
+    db "LARCH",0
+    db "WILLOW",0
+    db "SYCAMORE",0
+
+p36: db "NUTS",0
+    db "PECAN",0
+    db "CASHEW",0
+    db "COCONUT",0
+
+p37: db "FLOWERS",0
+    db "TULIP",0
+    db "BLUEBELL",0
+    db "DAFFODIL",0
+
+p38: db "UK MOUNTAINS",0
+    db "SNOWDON",0
+    db "BEN NEVIS",0
+    db "SCAFELL PIKE",0
+
+p39: db "UK RIVERS",0
+    db "THAMES",0
+    db "SEVERN",0
+    db "DERWENT",0
+
+p40: db "BIG RIVERS",0
+    db "VOLGA",0
+    db "AMAZON",0
+    db "MEKONG",0
+
+p41: db "CLOCKS",0
+    db "CUCKOO",0
+    db "SUNDIAL",0
+    db "GRANDFATHER",0
+
+p42: db "CLOTHES",0
+    db "JUMPER",0
+    db "SWEATER",0
+    db "CARDIGAN",0
+
+p43: db "CLOTHES",0
+    db "JEANS",0
+    db "SHORTS",0
+    db "TROUSERS",0
+
+p44: db "CLOTHES",0
+    db "SHIRT",0
+    db "BLOUSE",0
+    db "CROP TOP",0
+
+p45: db "ALCOHOL",0
+    db "VODKA",0
+    db "WHISKY",0
+    db "DRAMBUIE",0
+
+p46: db "SINGERS",0
+    db "ELVIS",0
+    db "PRINCE",0
+    db "MADONNA",0
+
+p47: db "KITCHIN APPLIANCES",0
+    db "FREEZER",0
+    db "TOASTER",0
+    db "AIR FRYER",0
+
+p48: db "OBSOLETE MEDIA",0
+    db "CD ROM",0
+    db "BETAMAX",0
+    db "FLOPPY DISK",0
+
+p49: db "BIRDS",0
+    db "THRUSH",0
+    db "SPARROW",0
+    db "BLACKBIRD",0
+
+p50: db "BIRDS",0
+    db "GROUSE",0
+    db "MOORHEN",0
+    db "PHEASANT",0
+
+    endmodule
