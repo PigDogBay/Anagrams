b0f73047ec00bf56671bc83b8bac6a13842f33c4
Mark Bailey <markpdbailey@gmail.com>
2025-09-16 11:32:28 +0100

    Added new puzzles to the nex file

--- src/Main.asm
+++ src/Main.asm
@@ -14,8 +14,12 @@
     ;  - Tile font Calstone
     ;  - Text font based on Truffle Shuffle
 
-BANK_CAT_MUSIC:                 equ 30
-BANK_CAT_SCIENCE:               equ 31
+BANK_CAT_FRESHERS:              equ 30
+BANK_CAT_MUSIC:                 equ 31
+BANK_CAT_SCIENCE:               equ 32
+BANK_CAT_FILM_TV:               equ 33
+BANK_CAT_WORLD:                 equ 33
+
 BANK_SPRITE:                    equ 40
 BANK_IMAGE_1:                   equ 50
 BANK_IMAGE_1_PALETTE:           equ 60
@@ -186,10 +190,16 @@ stack_top:
     INCBIN "assets/font.spr"
 
 
+    MMU 0,BANK_CAT_FRESHERS, 0x0000
+    include "puzzles/Freshers.asm"
     MMU 0,BANK_CAT_MUSIC, 0x0000
     include "puzzles/Music.asm"
     MMU 0,BANK_CAT_SCIENCE, 0x0000
     include "puzzles/Science.asm"
+    MMU 0,BANK_CAT_FILM_TV, 0x0000
+    include "puzzles/FilmTv.asm"
+    MMU 0,BANK_CAT_WORLD, 0x0000
+    include "puzzles/World.asm"
 
     ;Load sprite data in 8k banks 30 + 31. Banks placed in MMU slots 0 and 1 
     MMU 0 1,BANK_SPRITE, 0x0000
