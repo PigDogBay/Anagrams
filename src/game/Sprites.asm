f306ac560110f194893bda93dfb6ef12add90c7c
Mark Bailey <markpdbailey@gmail.com>
2025-10-03 13:50:27 +0100

    Added more sprites

--- src/game/Sprites.asm
+++ src/game/Sprites.asm
@@ -15,12 +15,14 @@ QUIT_BUTTON             equ 36
 PREVIOUS                equ 37
 NEXT                    equ 38
 BEER                    equ 39 
-BOOK                    equ 40
+CALCULATOR              equ 40
 REROLL                  equ 41
-CALCULATOR              equ 42
+BOOK                    equ 42
 NOTEPAD                 equ 43
 EYE                     equ 44
-SCHOLAR                 equ 45
+APPLE                   equ 45
+TAO                     equ 46
+TUTOR                   equ 47
 
 
 init:
