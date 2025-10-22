bd61c13e0b35bbcf9829c1d1712881467d3dfd62
Mark Bailey <markpdbailey@gmail.com>
2025-10-22 10:31:26 +0100

    Removed debug code that was left in

--- src/model/GamePhases.asm
+++ src/model/GamePhases.asm
@@ -21,7 +21,7 @@ PHASE_WIN       equ 2
 ; 
 ;-----------------------------------------------------------------------------------
 start:
-    ld hl,$0803
+    ld hl,$0101
     call YearTerm.select
 
     ;Set up the game settings based on the chosen college
