7bcf9133f001abb22e000d066974afec8a36db1d
Mark Bailey <markpdbailey@gmail.com>
2025-12-11 12:17:13 +0000

    Text now 'animated' in

--- src/game/states/Story.asm
+++ src/game/states/Story.asm
@@ -14,15 +14,44 @@
 TITLE_Y equ 10
 TEXT_Y equ 2
 
+STORY_STATE_START:                      equ 0
+STORY_STATE_PARA1:                      equ 1
+STORY_STATE_PARA2:                      equ 2
+STORY_STATE_PARA3:                      equ 3
+STORY_STATE_FROM:                       equ 4
+STORY_STATE_INSTRUCTION:                equ 5
+STORY_STATE_DONE:                       equ 6
+
+STATE_DELAY:                            equ 25
+
+storyState:
+    db 0
+stateCounter:
+    db STATE_DELAY
+
+stateJumpTable:
+    dw storyStart
+    dw storyPara1
+    dw storyPara2
+    dw storyPara3
+    dw storyFrom
+    dw storyInstruction
+    dw storyDone
+
+
 enter:
     L2_SET_IMAGE IMAGE_STORY
+    ld a, STORY_STATE_START
+    ld (storyState),a
+    
     call NextSprite.removeAll
     call SpriteList.removeAll
     ; First sprite always the mouse pointer so that it is on top
     call Game.addMouseSpritePointer
 
     call Sound.playWinMusic
-    jp printText
+    call Tilemap.clear
+    ret
 
 
 update:
@@ -31,7 +60,7 @@ update:
     cp MouseDriver.STATE_BACKGROUND_CLICKED
     jr z, .mousePressed
     call Game.updateSprites
-    ret
+    jr storyStateUpdate
 
 .mousePressed:
     call GamePhases.start
@@ -42,15 +71,45 @@ update:
     TRANSITION_SCREEN GS_START, IMAGE_MICHAELMAS
     ret
 
-printText:
-    call Tilemap.clear
 
-    ld e, TEXT_Y - 1: ld hl,dear
+storyStateUpdate:
+    ld a, (stateCounter)
+    dec a
+    jr z, .updateState:
+    ld (stateCounter),a
+    ret
+
+.updateState:
+    ld a, STATE_DELAY
+    ld (stateCounter),a
+    
+    ld a,(storyState)
+    ld hl, stateJumpTable
+    ; Add twice, as table is two bytes per entry
+    add hl,a
+    add hl,a
+    ; get jump entry
+    ld de,(hl)
+    ld hl,de
+    jp hl
+
+
+storyStart:
+    ld a, STORY_STATE_PARA1     
+    ld (storyState),a
+
+    ld d,1 : ld e, TEXT_Y - 1: ld hl,dear
     ld b,Tilemap.DARK_TEAL
-    ld d, 1 : call Print.setCursorPosition : call Print.printString
+    call Print.setCursorPosition : call Print.printString
+
+    ret
+
+storyPara1:
+    ld a, STORY_STATE_PARA2     
+    ld (storyState),a
 
     ld b,Tilemap.DARK_GREEN
-    ld e, TEXT_Y + 3: ld hl,para1Line1
+    ld d,1 : ld e, TEXT_Y + 3 : ld hl,para1Line1
     call Print.setCursorPosition : call Print.printString
     ld e, TEXT_Y + 4: ld hl,para1Line2
     call Print.setCursorPosition : call Print.printString
@@ -59,17 +118,28 @@ printText:
     ld e, TEXT_Y + 6: ld hl,para1Line4
     call Print.setCursorPosition : call Print.printString
 
+    ret
+
+storyPara2:
+    ld a, STORY_STATE_PARA3     
+    ld (storyState),a
 
     ld b,Tilemap.DARK_TEAL
-    ld e, TEXT_Y + 8: ld hl,para2Line1
+    ld d,1 : ld e, TEXT_Y + 8: ld hl,para2Line1
     call Print.setCursorPosition : call Print.printString
     ld e, TEXT_Y +9: ld hl,para2Line2
     call Print.setCursorPosition : call Print.printString
     ld e, TEXT_Y +10: ld hl,para2Line3
     call Print.setCursorPosition : call Print.printString
 
+    ret
+
+storyPara3:
+    ld a, STORY_STATE_FROM      
+    ld (storyState),a
+
     ld b,Tilemap.DARK_GREEN
-    ld e, TEXT_Y + 12: ld hl,para3Line1
+    ld d,1 : ld e, TEXT_Y + 12: ld hl,para3Line1
     call Print.setCursorPosition : call Print.printString
     ld e, TEXT_Y + 13: ld hl,para3Line2
     call Print.setCursorPosition : call Print.printString
@@ -78,23 +148,40 @@ printText:
     ld e, TEXT_Y + 15: ld hl,para3Line4
     call Print.setCursorPosition : call Print.printString
 
-    ld b,Tilemap.DARK_TEAL
+    ret
 
-    ld e, TEXT_Y + 19: ld hl,from1
+storyFrom:
+    ld a, STORY_STATE_INSTRUCTION
+    ld (storyState),a
+
+    ld b,Tilemap.DARK_TEAL
+    ld d,1 : ld e, TEXT_Y + 19: ld hl,from1
     call Print.setCursorPosition : call Print.printString
     ld e, TEXT_Y + 22: ld hl,from2
     call Print.setCursorPosition : call Print.printString
     ld e, TEXT_Y + 23: ld hl,from3
     call Print.setCursorPosition : call Print.printString
 
-    ld b,Tilemap.GREEN
+    ret
 
+storyInstruction:
+    ld a, STORY_STATE_DONE      
+    ld (storyState),a
+
+    ld b,Tilemap.GREEN
     ; Click to exit
     ld e, 29
     ld hl,startInstruction
     call Print.setCursorPosition
     call Print.printCentred
+
     ret
+
+storyDone:
+    ret
+
+
+
 
 dear:
     db "Dear Elf,",0
