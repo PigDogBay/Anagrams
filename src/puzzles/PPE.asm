15b88fdfc5e27c6450afb57c1b7619e37cc14c24
Mark Bailey <markpdbailey@gmail.com>
2025-12-31 13:34:47 +0000

    Added skeleton for PPE puzzle

---
+++ src/puzzles/PPE.asm
@@ -0,0 +1,266 @@
+;-----------------------------------------------------------------------------------
+; 
+; Data: Philosophy, Politics and Economics
+; 
+;-----------------------------------------------------------------------------------
+
+    module CatPPE
+
+table:
+    dw p1,p2,p3,p4,p5,p6,p7,p8,p9,p10
+    dw p11,p12,p13,p14,p15,p16,p17,p18,p19,p20
+    dw p21,p22,p23,p24,p25,p26,p27,p28,p29,p30
+    dw p31,p32,p33,p34,p35,p36,p37,p38,p39,p40
+    dw p41,p42,p43,p44,p45,p46,p47,p48,p49,p50
+
+p1: db "BUSINESS ORGANISATION TYPES",0
+    db "COMPANY",0
+    db "CORPORATION",0
+    db "CONGLOMERATE",0
+
+p2: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p3: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p4: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p5: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p6: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p7: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p8: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p9: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p10: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p11: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p12: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p13: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p14: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p15: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p16: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p17: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p18: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p19: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p20: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p21: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p22: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p23: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p24: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p25: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p26: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p27: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p28: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p29: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p30: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p31: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p32: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p33: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p34: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p35: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p36: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p37: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p38: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p39: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p40: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p41: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p42: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p43: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p44: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p45: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p46: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p47: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p48: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p49: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+p50: db "",0
+    db "\n",0
+    db "\n",0
+    db "\n",0
+
+    endmodule
