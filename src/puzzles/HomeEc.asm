8f4dbe587691c904a981c002b9d230e986a1df39
Mark Bailey <markpdbailey@gmail.com>
2026-07-09 14:19:31 +0100

    Home economics puzzles

--- src/puzzles/HomeEc.asm
+++ src/puzzles/HomeEc.asm
@@ -13,254 +13,254 @@ table:
     dw p31,p32,p33,p34,p35,p36,p37,p38,p39,p40
     dw p41,p42,p43,p44,p45,p46,p47,p48,p49,p50
 
-p1: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p1: db "STEW ON IT",0
+    db "LOBBY",0
+    db "CASSEROLE",0
+    db "LANCASHIRE\nHOTPOT",0
 
-p2: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p2: db "THE FULL ENGLISH",0
+    db "FRIED\nEGGS",0
+    db "MUSHROOMS",0
+    db "STREAKY\nBACON",0
 
-p3: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p3: db "CLOWN FARE?",0
+    db "BIG MAC",0
+    db "QUARTER\nPOUNDER",0
+    db "CHICKEN\nMCNUGGETS",0
 
-p4: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p4: db "BURGERTIME INGREDIENTS?",0
+    db "MR HOT DOG",0
+    db "MR PICKLE",0
+    db "CHEF\nPETER PEPPER",0
 
-p5: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p5: db "HOW DO YOU HAVE YOURS?",0
+    db "BOILED EGG",0
+    db "POACHED\nEGG",0
+    db "SCRAMBLED\nEGGS",0
 
-p6: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p6: db "BREAKFAST TIME",0
+    db "FULL\nENGLISH",0
+    db "SMOKED\nKIPPERS",0
+    db "CONTINENTAL",0
 
-p7: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p7: db "US CEREAL KILLERS",0
+    db "CAP N\nCRUNCH",0
+    db "LUCKY\nCHARMS",0
+    db "GOLDEN\nGRAHAMS\nCRUNCH",0
 
-p8: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p8: db "UK CEREAL KILLERS",0
+    db "COCO POPS",0
+    db "FROSTIES",0
+    db "HONEY\nPOPS",0
 
-p9: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p9: db "...ON TOAST",0
+    db "BAKED\nBEANS",0
+    db "AVOCARDO",0
+    db "RED\nLEICESTER",0
 
-p10: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p10: db "ON THE CHEESE BOARD",0
+    db "CHUTNEY",0
+    db "CRACKERS",0
+    db "STINKING\nBISHOP",0
 
-p11: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p11: db "SANDWICH FILLINGS",0
+    db "HAM AND\nCHEESE",0
+    db "TUNA\nMAYONNAISE",0
+    db "CORONATION\nCHICKEN",0
 
-p12: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p12: db "COOKING ON THE BOX",0
+    db "CANT COOK\nWONT COOK",0
+    db "MASTERCHEF",0
+    db "SATURDAY\nKITCHEN",0
 
-p13: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p13: db "TWO MANY COOKS?",0
+    db "TWO FAT\nLADIES",0
+    db "THE HAIRY\nBIKERS",0
+    db "FANNY AND\nJOHNNIE\nCRADDOCK",0
 
-p14: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p14: db "MEATY CURRIES",0
+    db "BEEF\nMADRAS",0
+    db "MUTTON\nVINDALOO",0
+    db "CHICKEN\nTIKKA\nMASALA",0
 
-p15: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p15: db "BURTON BREWS",0
+    db "CARLING",0
+    db "BASS\nPALE ALE",0
+    db "MARSTONS\nPEDIGREE",0
 
-p16: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p16: db "BREWERS - LEST WE FORGET",0
+    db "ANSELLS",0
+    db "TETLEY\nWALKER",0
+    db "BODDINGTONS\n",0
 
-p17: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p17: db "BREWERS - LEST WE FORGET",0
+    db "YOUNGERS",0
+    db "BRAKSPEAR",0
+    db "SCOTTISH\nAND\nNEWCASTLE",0
 
-p18: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p18: db "PIZZA",0
+    db "PEPPERONI",0
+    db "MARGHERITA",0
+    db "HAWAIIAN",0
 
-p19: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p19: db "PIZZA TOPPINGS",0
+    db "PINEAPPLE",0
+    db "BELL\nPEPPERS",0
+    db "ANCHOVIES",0
 
-p20: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p20: db "A NICE CUP OF TEA",0
+    db "PG TIPS",0
+    db "YORKSHIRE",0
+    db "ENGLISH\nBREAKFAST",0
 
-p21: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p21: db "A NICE CUP OF TEA",0
+    db "EARL GREY",0
+    db "CHAMOMILE",0
+    db "DARJEELING",0
 
-p22: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p22: db "SWEENEYS FARE?",0
+    db "STEAK\nAND ALE",0
+    db "MEAT AND\nPOTATO",0
+    db "CHICKEN\nAND LEEK",0
 
-p23: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p23: db "FOUND IN GREGGS",0
+    db "STEAK BAKE",0
+    db "SAUSAGE\nROLL",0
+    db "BACON\nBREAKFAST\nROLL",0
 
-p24: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p24: db "BAKERS DELIGHTS",0
+    db "CREAM\nHORN",0
+    db "JAM\nDOUGHNUT",0
+    db "VANILLA\nSLICE",0
 
-p25: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p25: db "CLASSIC UK DESSERTS",0
+    db "BRIDS\nTRIFLE",0
+    db "ANGEL\nDELIGHT",0
+    db "WALLS\nVIENNETTA",0
 
-p26: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p26: db "OLD SKOOL DESSERTS?",0
+    db "SPOTTED\nDICK",0
+    db "CAKE AND\nCUSTARD",0
+    db "SEMOLINA\nPUDDING",0
 
-p27: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p27: db "KIDS PARTY FARE",0
+    db "CRISPS",0
+    db "BIRTHDAY\nCAKE",0
+    db "JELLY AND\nICECREAM",0
 
-p28: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p28: db "JUST DESSERTS",0
+    db "TIRAMISU",0
+    db "CREME\nBRULEE",0
+    db "BLANCMANGE",0
 
-p29: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p29: db "FOOD FOR US?",0
+    db "APPLE PIE",0
+    db "HAMBURGER\nAND FRIES",0
+    db "PHILLY\nCHEESESTEAK",0
 
-p30: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p30: db "LE CUISINE",0
+    db "CROISSANT",0
+    db "BAGUETTE",0
+    db "BOEUF\nBOURGUIGNON",0
 
-p31: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p31: db "FOUND IN EL RESTAURANTE",0
+    db "CALAMARES",0
+    db "PAELLA\nNEGRA",0
+    db "CHORIZO\nTAPAS",0
 
-p32: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p32: db "ICE COLD CERVEZA",0
+    db "SAN MIGUEL",0
+    db "CRUZCAMPO",0
+    db "ESTRELLA\nDAMM",0
 
-p33: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p33: db "DRINKERS SUSTENANCE",0
+    db "DONER\nKEBAB",0
+    db "BAG OF\nCRISPS",0
+    db "ROASTED\nPEANUTS",0
 
-p34: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p34: db "THESE GUYS ARE FUN!",0
+    db "CHESTNUT",0
+    db "SHIITAKE",0
+    db "PORTOBELLO",0
 
-p35: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p35: db "THE STAKES WERE TOO HIGH?",0
+    db "SIRLOIN",0
+    db "FILET\nMIGNON",0
+    db "PORTERHOUSE",0
 
-p36: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p36: db "SAUSAGE FEST",0
+    db "WIENER",0
+    db "CURRYWURST",0
+    db "FRANKFURTER",0
 
-p37: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p37: db "SOME BANGERS HERE",0
+    db "BLACK\nPUDDING",0
+    db "CUMBERLAND\nRING",0
+    db "CHIPOLATAS",0
 
-p38: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p38: db "YOU DO WIN FRIENDS WITH...",0
+    db "WALDORF",0
+    db "NICOISE",0
+    db "TABBOULEH",0
 
-p39: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p39: db "IT'S ALL HELLENIC TO ME",0
+    db "GREEK\nSALAD",0
+    db "MOUSSAKA",0
+    db "TARAMASALATA",0
 
-p40: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p40: db "LITTLE FISHY ON A DISHY",0
+    db "TUNA\nMELT",0
+    db "BATTERED\nCOD",0
+    db "SMOKED\nSALMON",0
 
-p41: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p41: db "BURNS FOOD?",0
+    db "NEEPS AND\nTATTIES",0
+    db "CULLEN\nSKINK",0
+    db "FINNAN\nHADDIE",0
 
-p42: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p42: db "EMERALD ISLE DELIGHTS",0
+    db "BOXTY",0
+    db "GUINNESS",0
+    db "COLCANNON",0
 
-p43: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p43: db "AT THE CHIPPY",0
+    db "MUSHY\nPEAS",0
+    db "SALT AND\nVINEGAR",0
+    db "SAVELOY\nSAUSAGE",0
 
-p44: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p44: db "THE LADY LOVES MILK TRAY",0
+    db "CARAMEL\nSOFTY",0
+    db "HAZELNUT\nSWIRL",0
+    db "ORANGE\nTRUFFLE",0
 
-p45: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p45: db "THIS TAKES THE BISCUIT",0
+    db "PENGUIN",0
+    db "JAMMY\nDODGER",0
+    db "CUSTARD\nCREAM",0
 
-p46: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p46: db "DUNKERS?",0
+    db "HOBNOBS",0
+    db "BOURBON\nBISCUIT",0
+    db "CHOCOLATE\nDIGESTIVE",0
 
-p47: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p47: db "BUTTER ME UP?",0
+    db "CRUMPET",0
+    db "SANDWICH",0
+    db "BAKED\nPOTATO",0
 
-p48: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p48: db "USE YOUR LOAF?",0
+    db "BRIOCHE",0
+    db "CIABATTA",0
+    db "SOURDOUGH",0
 
-p49: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p49: db "FLAT BREADS",0
+    db "BANNOCK",0
+    db "CHAPATI",0
+    db "TORTILLA",0
 
-p50: db "",0
-    db "\n",0
-    db "\n",0
-    db "\n",0
+p50: db "NOT FOR SANDWICHES!",0
+    db "SHORTBREAD",0
+    db "SWEETBREAD",0
+    db "LAVERBREAD",0
 
     endmodule
